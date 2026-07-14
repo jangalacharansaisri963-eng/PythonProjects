e263590a8914e184a0c41c3aa521a88c0e454d81
jangalacharansaisri963-eng <jangalacharansaisri963@gmail.com>
2026-07-14 22:05:55 +0530

    Add Tokenizer header file for expression tokenization

---
+++ C++/ScientificCalculator/src/Tokenizer.h
@@ -0,0 +1,18 @@
+#ifndef TOKENIZER_H
+#define TOKENIZER_H
+
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+
+// Converts input expression into tokens
+
+vector<string> tokenize(
+    const string& expression
+);
+
+
+#endif
