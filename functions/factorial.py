6b63800903fd91fde2183b98fffbace9e7a8e24b
jangalacharansaisri963-eng <jangalacharansaisri963@gmail.com>
2026-07-14 19:26:31 +0530

    Validate input for factorial function
    Add input validation to ensure x is an integer.

--- functions/factorial.py
+++ functions/factorial.py
@@ -9,4 +9,9 @@ import math
 
 def factorial(x):
 
+    if int(x) != x:
+        raise ValueError(
+            "Factorial is only defined for integers."
+        )
+
     return math.factorial(int(x))
