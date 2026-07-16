e139b3cf618abe932df02ed46d76a3ddc8153428
jangalacharansaisri963-eng <jangalacharansaisri963@gmail.com>
2026-07-16 19:37:32 +0530

    Add mathematical constants PI and E

---
+++ Java/ScientificCalculator/src/constants/Constants.java
@@ -0,0 +1,19 @@
+package constants;
+
+
+public class Constants {
+
+
+    // ==========================
+    // Mathematical Constants
+    // ==========================
+
+    public static final double PI =
+            Math.PI;
+
+
+    public static final double E =
+            Math.E;
+
+
+}
