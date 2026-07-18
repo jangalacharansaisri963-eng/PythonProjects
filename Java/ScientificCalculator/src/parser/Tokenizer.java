8115f5c6ebb10c13671e738e9d489fd20f21daab
jangalacharansaisri963-eng <jangalacharansaisri963@gmail.com>
2026-07-18 06:07:18 +0530

    Refactor tokenize method for improved readability

--- Java/ScientificCalculator/src/parser/Tokenizer.java
+++ Java/ScientificCalculator/src/parser/Tokenizer.java
@@ -3,126 +3,83 @@ package parser;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Tokenizer {
-
 
     // ==========================
     // Tokenize Expression
     // ==========================
 
-    public static List<String> tokenize(
-            String expression
-    ) {
+    public static List<String> tokenize(String expression) {
 
-        List<String> tokens =
-                new ArrayList<>();
+        List<String> tokens = new ArrayList<>();
 
-
-        StringBuilder current =
-                new StringBuilder();
-
-
+        StringBuilder current = new StringBuilder();
 
         for (int i = 0; i < expression.length(); i++) {
 
-
-            char c =
-                    expression.charAt(i);
-
-
+            char c = expression.charAt(i);
 
             // ==========================
             // Numbers / Names
             // ==========================
 
-            if (
-                Character.isDigit(c)
-                ||
-                c == '.'
-                ||
-                Character.isLetter(c)
-            ) {
+            if (Character.isDigit(c)
+                    || c == '.'
+                    || Character.isLetter(c)) {
 
                 current.append(c);
                 continue;
             }
 
-
-
             // Finish current token
 
             if (current.length() > 0) {
 
-                tokens.add(
-                    current.toString()
-                );
-
+                tokens.add(current.toString());
                 current.setLength(0);
 
             }
-
-
 
             // ==========================
             // Spaces
             // ==========================
 
-            if (
-                Character.isWhitespace(c)
-            ) {
-
+            if (Character.isWhitespace(c)) {
                 continue;
-
             }
 
-
-
             // ==========================
-            // Operators + Brackets
+            // Operators / Brackets / Comma
             // ==========================
 
             if (
-                c == '+'
-                ||
-                c == '-'
-                ||
-                c == '*'
-                ||
-                c == '/'
-                ||
-                c == '('
-                ||
-                c == ')'
+                    c == '+'
+                    || c == '-'
+                    || c == '*'
+                    || c == '/'
+                    || c == '^'
+                    || c == '('
+                    || c == ')'
+                    || c == ','
             ) {
 
-                tokens.add(
-                    String.valueOf(c)
-                );
+                tokens.add(String.valueOf(c));
 
-            }
-
-            else {
+            } else {
 
                 throw new RuntimeException(
-                    "Unknown character: " + c
+                        "Unknown character: " + c
                 );
 
             }
 
         }
-
-
 
         if (current.length() > 0) {
 
-            tokens.add(
-                current.toString()
-            );
+            tokens.add(current.toString());
 
         }
-
-
 
         return tokens;
 
